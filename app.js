b955716f54bb750f13af2751dcb863ca66020445
ZhouChenxing-Joe <w86311525@gmail.com>
2024-11-13 21:43:32 +0800

    Version 1

---
+++ app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+// where you write data, functions inside here.
+   // if more complex html we use template it might get mess ==> template: '<h2>This is template</h2>'
+   // so we just create in html file div#app
+
+
+   data() {
+      return {
+        title: 'This ME',
+        author: 'Zhou Joe',
+        AGE: '19',
+      }
+
+   }
+
+})
+
+
+app.mount('#app')
